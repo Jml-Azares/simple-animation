37d2c0bad7f9eb35e1cbf50affe5045cca2f5535
Joseph Moroni L. Azares <133185546+Jml-Azares@users.noreply.github.com>
2023-05-30 14:32:13 +0800

    clean code

--- index.js
+++ index.js
@@ -6,37 +6,30 @@ const learnMoreImage = document.querySelector(".learnmore");
 const logoImage = document.querySelector(".logo");
 const replayImage = document.querySelector(".replay");
 
-// Fade out the female image after 3 seconds
 setTimeout(() => {
   femaleImage.classList.add("fade-out");
 }, 3000);
 
-// Make headline2 image visible after 5 seconds
 setTimeout(() => {
   headline2Image.style.opacity = "1";
 }, 3000);
 
-// Scale down and move headline1 image to the right corner after 6 seconds
 setTimeout(() => {
   headline1Image.classList.add("scale-out");
 }, 5000);
 
-// Make subheadline image visible after 6 seconds
 setTimeout(() => {
   subheadlineImage.style.opacity = "1";
 }, 7000);
 
-// Make learnmore image visible after 9 seconds
 setTimeout(() => {
   learnMoreImage.style.opacity = "1";
 }, 9000);
 
-// Move the logo image from left to right after 9 seconds
 setTimeout(() => {
   logoImage.style.opacity = "1";
 }, 9000);
 
-// Make replay image visible after 12 seconds
 setTimeout(() => {
   replayImage.style.opacity = "1";
 }, 12000);
